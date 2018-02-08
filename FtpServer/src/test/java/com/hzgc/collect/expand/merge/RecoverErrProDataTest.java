b5bd28b6bdd2250b5773b5704f49e6148387a2eb
sukidasakura <mashencai@126.com>
2018-02-08 10:57:42 +0800

    edit test

--- FtpServer/src/test/java/com/hzgc/collect/expand/merge/RecoverErrProDataTest.java
+++ FtpServer/src/test/java/com/hzgc/collect/expand/merge/RecoverErrProDataTest.java
@@ -8,17 +8,43 @@ import com.hzgc.collect.expand.util.JSONHelper;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 import static org.junit.Assert.*;
 
 public class RecoverErrProDataTest {
+
+
+    /**
+     * Auxiliary tools
+     * copy files
+     */
+    public void copyFile(String sourceFile, String destinationFile) throws IOException {
+        if (sourceFile != null && destinationFile != null
+                && !Objects.equals(sourceFile, "")
+                && !Objects.equals(destinationFile, "")) {
+            if (new File(sourceFile).exists() && new File(sourceFile).isFile()) {
+                File destinationFolder = new File(destinationFile).getParentFile();
+                if (!destinationFolder.exists()) {
+                    destinationFolder.mkdirs();
+                }
+                //拷贝文件。REPLACE_EXISTING: 如果目标文件存在，则替换。如果不存在，则移动。
+                Files.copy(Paths.get(sourceFile), Paths.get(destinationFile),
+                        StandardCopyOption.REPLACE_EXISTING);
+            }
+        }
+    }
+
+
     private MergeUtil mergeUtil = new MergeUtil();
     private Logger LOG = Logger.getLogger(RecoverErrProDataTest.class);
 
@@ -32,39 +58,49 @@ public class RecoverErrProDataTest {
     private String SUFFIX = ".log";
     private SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd");
 
+    private String backupDir = "/home/test/ftp/backup/";
+
     /**
      * 测试获取process下错误日志的锁，并移动到success和merge的部分。
      */
     @Test
-    public void testLockAndMove() {
+    public void testLockAndMove() throws IOException {
 
         System.out.println(("************************************" +
                 "testLockAndMove：将process目录下所有能获取到锁的error日志，移动到success和merge" +
                 "************************************"));
         List<String> allErrorDir = mergeUtil.listAllErrorLogAbsPath(processLogDir);
-        for (String errFile : allErrorDir) {
+        for (int i = 0; i < allErrorDir.size(); i++) {
             //获取每个error.log需要移动到的success和merge目录下的路径
-            String successErrFile = mergeUtil.getSuccessFilePath(errFile);
-            String mergeErrFile = mergeUtil.getMergeFilePath(errFile);
-            System.out.println("每个error.log对应的success备份路径和merge处理路径：");
-            System.out.println(errFile);
+            String successErrFile = mergeUtil.getSuccessFilePath(allErrorDir.get(i));
+            String mergeErrFile = mergeUtil.getMergeFilePath(allErrorDir.get(i));
+            System.out.println("*************The " + i + " error.log对应的success备份路径和merge处理路径：**************");
+            System.out.println(allErrorDir.get(i));
             System.out.println(successErrFile);
             System.out.println(mergeErrFile);
             //移动前，每个errorFile中的内容
-            List<String> errContent1 = mergeUtil.getAllContentFromFile(errFile);
+            List<String> errContentBefore = mergeUtil.getAllContentFromFile(allErrorDir.get(i));
 
             //移动到merge后，拷贝一份到success
-            mergeUtil.lockAndMove(errFile, mergeErrFile); //其中包括判断锁是否存在
+            mergeUtil.lockAndMove(allErrorDir.get(i), mergeErrFile); //其中包括判断锁是否存在
             mergeUtil.copyFile(mergeErrFile, successErrFile);
 
             //移动后，每个errorFile中的内容
-            List<String> errContent2 = mergeUtil.getAllContentFromFile(errFile);
-            assertEquals("移动后，原error日志是否为空：", 0, errContent2.size());
-            List<String> errContent3 = mergeUtil.getAllContentFromFile(mergeErrFile);
-            assertArrayEquals("移动后，merge/error日志是否与原日志内容相同：", errContent1.toArray(), errContent3.toArray());
-            List<String> errContent4 = mergeUtil.getAllContentFromFile(successErrFile);
-            assertArrayEquals("移动后，success/error日志是否与原日志内容相同：", errContent1.toArray(), errContent4.toArray());
-            assertArrayEquals("测试assertArrayEquals是否能够比较两个List是否相同：", errContent1.toArray(), errContent2.toArray());
+            List<String> errContentAfter = mergeUtil.getAllContentFromFile(allErrorDir.get(i));
+            assertEquals("移动后，原error日志是否为空：", 0, errContentAfter.size());
+            List<String> errContentMerge = mergeUtil.getAllContentFromFile(mergeErrFile);
+            assertEquals("移动后，merge/error日志是否与原日志内容相同：", errContentBefore, errContentMerge);
+            List<String> errContentSuc = mergeUtil.getAllContentFromFile(successErrFile);
+            assertEquals("移动后，success/error日志是否与原日志内容相同：", errContentBefore, errContentSuc);
+            //assertNotEquals("移动后，success/error日志与原日志", errContent1, errContent4);
+            System.out.println(errContentBefore);
+            System.out.println(errContentAfter);
+            System.out.println(errContentMerge);
+            System.out.println(errContentSuc);
+
+            //for next time test
+            copyFile(backupDir + "error1.log", processLogDir + "/p-0/error/error.log");
+            copyFile(backupDir + "error2.log", processLogDir + "/p-1/error/error.log");
 
         }
     }
@@ -75,6 +111,7 @@ public class RecoverErrProDataTest {
      * 假设每个error.log的前两条发送kafka失败，看是否能够写入到merge/error下的新日志中。
      * 不包含发送kafka
      */
+    @Test
     public void testDealMergeError() throws IOException {
 
         System.out.println(("************************************" +
@@ -121,12 +158,12 @@ public class RecoverErrProDataTest {
                 errorTwo.add(errorRows.get(0));
                 errorTwo.add(errorRows.get(1));
                 //error.log的前两行新写入的error-N，放入List中
-                List<String> mergeErrFileNewList =Files.readAllLines(Paths.get(mergeErrFileNew));
+                List<String> mergeErrFileNewList = Files.readAllLines(Paths.get(mergeErrFileNew));
                 //比较新写入的mergeErrFileNew，是否和error.log中的前两行相同
-                assertArrayEquals("比较新写入的mergeErrFileNew，是否和error.log中的前两行相同：",errorRows.toArray(), mergeErrFileNewList.toArray());
+                assertArrayEquals("比较新写入的mergeErrFileNew，是否和error.log中的前两行相同：", errorRows.toArray(), mergeErrFileNewList.toArray());
                 mergeUtil.deleteFile(errorFilePath); //删除已处理过的error日志
             }
-        }else{ //若merge/error目录下无日志
+        } else { //若merge/error目录下无日志
             LOG.info("Nothing in " + mergeErrLogDir);
         }
     }
